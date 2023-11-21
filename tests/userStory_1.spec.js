ec1d38b0915bc960e7ce8878d960bd75e943b49d
ranam2030 <masudr2030@gmail.com>
2023-11-22 00:40:46 +0600

    Com Shop

--- tests/userStory_1.spec.js
+++ tests/userStory_1.spec.js
@@ -4,13 +4,7 @@ const { test, expect } = require('@playwright/test');
 test('User able to click the Add a new computer button', async ({ page }) => {
   await page.goto('https://computer-database.gatling.io/computers');
   await page.click('id=add');
-  // await page.locator('#name').fill("Pacfic");
-  // await page.locator('#introduced').fill("01 Jan 1919");
-  // await page.locator('#discontinued').fill("20 Aug 2019");
-  // await page.locator('#company').click();
-  // await page.click("text=Netronics");
   await expect(page.getByText('Add a computer')).toBeVisible();
-  //await page.click('//input[@value="Create this computer"]');
 });
 
 
